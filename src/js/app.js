109e8ccad2dc2b26c15869c60266d42d3931723d
retani <holgheiss@gmail.com>
2015-01-26 23:58:08 +0100

    try slide animation

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,29 @@
-angular.module('K2020', [
+var app = angular.module('K2020', [
   'ngRoute',
   'mobile-angular-ui',
   'K2020.controllers.Main',
-  'ngSanitize'
+  'ngSanitize',
+  'ngAnimate'
 ])
 
 .config(function($routeProvider) {
-  $routeProvider.when('/', {templateUrl:'home.html',  reloadOnSearch: false});
-  $routeProvider.when('/task', {templateUrl: 'task.html', reloadOnSearch: false}); 
-  $routeProvider.when('/about', {templateUrl: 'about.html', reloadOnSearch: false}); 
+  $routeProvider.when('/', {templateUrl:'home.html',  reloadOnSearch: false, depth: 1});
+  $routeProvider.when('/task', {templateUrl: 'task.html', reloadOnSearch: false, depth: 2}); 
+  $routeProvider.when('/about', {templateUrl: 'about.html', reloadOnSearch: false, depth: 2}); 
 });
+
+app.run(function($rootScope, $window) {
+  // publish current transition direction on rootScope
+  $rootScope.direction = 'ltr';
+  // listen change start events
+  $rootScope.$on('$routeChangeStart', function(event, next, current) {
+    $rootScope.direction = 'rtl';
+    console.log(arguments);
+    if (current && next && (current.depth > next.depth)) {
+      $rootScope.direction = 'ltr';  
+    }
+    // back
+    $rootScope.back = function() {
+      $window.history.back();
+    }});
+  });
